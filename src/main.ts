3b00a96a045a252bcc171cd3d86b746abf94af57
Gabriel Pupim <gabriel.pda15@gmail.com>
2023-01-25 01:16:51 -0300

    feat: added base electron functionality

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,52 @@
-console.log('Hello Feeders!');
+import 'reflect-metadata';
+import { resolve as pathResolve } from 'path';
+import { app as electronApp, BrowserWindow } from 'electron';
+
+const app: {
+	main_window?: BrowserWindow;
+} = {};
+
+function createMainWindow() {
+	app.main_window = new BrowserWindow({
+		width: 1280,
+		height: 720,
+		frame: false,
+		resizable: true,
+		webPreferences: {
+			nodeIntegration: true,
+			nodeIntegrationInWorker: true,
+			contextIsolation: false,
+		},
+	});
+
+	const url = {
+		pathname: pathResolve(__dirname, 'index.html'),
+		protocol: 'file',
+	};
+
+	app.main_window.loadURL(`${url.protocol}://${url.pathname}`);
+
+	if (process.env.DEBUG === 'true') {
+		app.main_window.webContents.openDevTools();
+	}
+
+	app.main_window.on('closed', () => {
+		app.main_window = null;
+	});
+}
+
+electronApp.on('ready', async () => {
+	createMainWindow();
+
+	const remote = await import('@electron/remote/main');
+	remote.initialize();
+	remote.enable(app.main_window.webContents);
+});
+
+electronApp.on('window-all-closed', () => {
+	if (process.platform !== 'darwin') electronApp.quit();
+});
+
+electronApp.on('activate', () => {
+	if (!app.main_window) createMainWindow();
+});
